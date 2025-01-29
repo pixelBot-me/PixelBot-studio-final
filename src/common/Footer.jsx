ce475b9e60d325f3b7b38c832b6ae7edd5f54d57
Nikita Kose <nikita@thepixelbot.com>
2025-01-29 14:35:41 +0530

    addd a tag text in footer

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -4,93 +4,91 @@ import 'bootstrap/dist/css/bootstrap.css';
 import '../assets/css/footer.css'
 
 export default function Footer() {
-  return (
-   <>
-   {/* Footer */}
-   <footer className="footer">
-        <Container>
-          <div className="footer-row">
-            <div className="footer-col py-3">
-                <h4>Our Offices</h4>
-              <div className="footer-subcol">India</div>
-              <div className="footer-subcol">USA 2</div>
-              <div className="footer-subcol">UAE</div>
-            </div>
-            <div className="footer-col">
-                <h4>abc@gmail.com</h4>
-            </div>
-          </div>
-          <div className="footer-row py-3">
-            <div className="footer-col">
-                <div>
-                Services
+    return (
+        <>
+            {/* Footer */}
+            <footer className="footer">
+                <Container>
+                    <div className="footer-row">
+                        <div className="footer-col py-3">
+                            <h4>Our Offices</h4>
+                            <div className="footer-subcol">India</div>
+                            <div className="footer-subcol">USA 2</div>
+                            <div className="footer-subcol">UAE</div>
+                        </div>
+                        <div className="footer-col">
+                            <h4>abc@gmail.com</h4>
+                        </div>
                     </div>
-                    <div>
-                Services
-                    </div>  <div>
-                Services
-                    </div>  <div>
-                Services
-                    </div>  <div>
-                Services
+                    <div className="footer-row py-3">
+                        <div className="footer-col">
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Services</a>
+                            </div>
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Services</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Services</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Services</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Services</a>
+                            </div>
+
+
+                        </div>
+                        <div className="footer-col">
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Hire Developers</a>
+                            </div>
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Hire Developers</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Hire Developers</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Hire Developers</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Hire Developers</a>
+                            </div>
+
+
+                        </div>
+                        <div className="footer-col">
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Learn</a>
+                            </div>
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Learn</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Learn</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Learn</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Learn</a>
+                            </div>                            
+                        </div>
+                        <div className="footer-col">
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Solutions</a>
+                            </div>
+                            <div>
+                                <a href="#" className='text-decoration-none text-white'>Solutions</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Solutions</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Solutions</a>
+                            </div>  <div>
+                                <a href="#" className='text-decoration-none text-white'>Solutions</a>
+                            </div>       
+                        </div>
                     </div>
+                    <div className="footer-bottom py-3">
+                        <p>&copy; 2025 Your Website</p>
+                        <p>Privacy Policy</p>
+                    </div>
+                </Container>
+            </footer>
 
-
-            </div>
-            <div className="footer-col">
-
-                <div>
-                Hire Developers
-                </div>
-                <div>
-                Hire Developers
-                </div><div>
-                Hire Developers
-                </div><div>
-                Hire Developers
-                </div><div>
-                Hire Developers
-                </div>
-                
-            </div>
-            <div className="footer-col">
-                <div>
-                Learn
-                </div>
-                <div>
-                Learn
-                </div> <div>
-                Learn
-                </div> <div>
-                Learn
-                </div> <div>
-                Learn
-                </div>
-            </div>
-            <div className="footer-col">
-                <div>
-                Solutions
-                </div>
-                <div>
-                Solutions
-                </div><div>
-                Solutions
-                </div><div>
-                Solutions
-                </div><div>
-                Solutions
-                </div><div>
-                Solutions
-                </div>
-            </div>
-          </div>
-          <div className="footer-bottom py-3">
-            <p>&copy; 2025 Your Website</p>
-            <p>Privacy Policy</p>
-          </div>
-        </Container>
-      </footer>
-   
-   </>
-  )
+        </>
+    )
 }
