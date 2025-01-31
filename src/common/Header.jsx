326272bcc8b3fe4de81582cb1371e749893e8f1f
Nikita Kose <nikita@thepixelbot.com>
2025-01-31 15:10:12 +0530

    make submenu statically

--- src/common/Header.jsx
+++ src/common/Header.jsx
@@ -25,7 +25,6 @@ export default function Header() {
                 {/* Top Bar */}
                 {!scrolling && (
                     <div className="top-bar d-flex justify-content-center">
-                        {/* <p className="text-center">Welcome to Our Website</p> */}
                         <div>
                             📩 &nbsp;
                             <a href="mailto:info@pixelbotstudio.com" className="text-decoration-none text-white">info@pixelbotstudio.com  &nbsp;| </a>
@@ -49,104 +48,140 @@ export default function Header() {
                         <Navbar.Toggle onClick={() => setMobileMenu(!mobileMenu)} />
                         <Navbar.Collapse className={mobileMenu ? "show" : ""}>
                             <Nav className="mx-auto">
-                                {["Home", "About", "Services", "Portfolio", "Blog", "Contact"].map(
-                                    (item, index) => (
-                                        <div
-                                            key={index}
-                                            className="nav-item"
-                                            onMouseEnter={() => setMenuOpen(index)}
-                                            onMouseLeave={() => setMenuOpen(null)}
-                                        >
-                                            <Nav.Link href="#" className="menu-link">
-                                                {item}
-                                            </Nav.Link>
-                                            {menuOpen === index && (
-                                                <div className="submenu d-flex">
-
-                                                    <div className="submenu-item">
-                                                        <h5>The Company</h5>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none">Option 1</a>
-                                                        </div>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div>
-
-
-                                                    </div>
-                                                    <div className="submenu-item">
-                                                        <h5>The Company</h5>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none">Option 1</a>
-                                                        </div>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div>
-
-
-                                                    </div><div className="submenu-item">
-                                                        <h5>The Company</h5>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none">Option 1</a>
-                                                        </div>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div>
-                                                        <div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div><div className="py-1">
-
-                                                            <a href="" className="text-dark text-decoration-none ">Option 1</a>
-                                                        </div>
-
-
-                                                    </div>
-
-
-                                                </div>
-                                            )}
+                                {/* Static Nav Items */}
+                                <div
+                                    className="nav-item"
+                                    onMouseEnter={() => setMenuOpen(0)}
+                                    onMouseLeave={() => setMenuOpen(null)}
+                                >
+                                    <Nav.Link href="#" className="menu-link">
+                                        Company &darr;
+                                    </Nav.Link>
+                                    {menuOpen === 0 && (
+                                        <div className="submenu d-flex flex-column">
+                                        <h4 className="text-danger mb-4">The Company</h4>
+                                        {/* Header Row */}
+                                    
+                                        {/* First Column Section */}
+                                        <div className="submenu-row d-flex flex-column mb-4">
+                                            <a href="" className="text-dark text-decoration-none py-2">About Us</a>
+                                            <a href="" className="text-dark text-decoration-none py-2">Clients</a>
+                                            <a href="" className="text-dark text-decoration-none py-2">Careers</a>
                                         </div>
-                                    )
-                                )}
+                                    
+                                        {/* Second Column Section */}
+                                        <div className="submenu-column d-flex flex-column ">
+                                            <a href="" className="text-dark text-decoration-none mb-3">Imagination Meets, Implementation</a>
+                                            <div className="d-flex justify-content-between">
+                                                <div className="stats-item text-center">
+                                                    <h2>9+</h2>
+                                                    <p>Years in Business</p>
+                                                </div>
+                                                <div className="stats-item text-center">
+                                                    <h2>100+</h2>
+                                                    <p>Dummy Text</p>
+                                                </div>
+                                            </div>
+                                            <div className="d-flex justify-content-between mt-3">
+                                                <div className="stats-item text-center">
+                                                    <h2>9+</h2>
+                                                    <p>Years in Business</p>
+                                                </div>
+                                                <div className="stats-item text-center">
+                                                    <h2>100+</h2>
+                                                    <p>Dummy Text</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    )}
+                                </div>
+                                <div
+                                    className="nav-item"
+                                    onMouseEnter={() => setMenuOpen(1)}
+                                    onMouseLeave={() => setMenuOpen(null)}
+                                >
+                                    <Nav.Link href="#" className="menu-link">
+                                        About
+                                    </Nav.Link>
+                                    {menuOpen === 1 && (
+                                        <div className="submenu d-flex">
+                                            <div className="submenu-item">
+                                                <a href="" className="text-dark text-decoration-none">Option 1</a>
+                                            </div>
+                                            <div className="submenu-item">
+                                                <a href="" className="text-dark text-decoration-none">Option 2</a>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                                <div
+                                    className="nav-item"
+                                    onMouseEnter={() => setMenuOpen(2)}
+                                    onMouseLeave={() => setMenuOpen(null)}
+                                >
+                                    <Nav.Link href="#" className="menu-link">
+                                        Services
+                                    </Nav.Link>
+                                    {menuOpen === 2 && (
+                                        <div className="submenu d-flex">
+                                            <div className="submenu-item">
+                                                <a href="" className="text-dark text-decoration-none">Option 1</a>
+                                            </div>
+                                            <div className="submenu-item">
+                                                <a href="" className="text-dark text-decoration-none">Option 2</a>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                                <div
+                                    className="nav-item"
+                                    onMouseEnter={() => setMenuOpen(3)}
+                                    onMouseLeave={() => setMenuOpen(null)}
+                                >
+                                    <Nav.Link href="#" className="menu-link">
+                                        Portfolio
+                                    </Nav.Link>
+                                    {menuOpen === 3 && (
+                                        <div className="submenu d-flex">
+                                            <div className="submenu-item">
+                                                <a href="" className="text-dark text-decoration-none">Option 1</a>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                                <div
+                                    className="nav-item"
+                                    onMouseEnter={() => setMenuOpen(4)}
+                                    onMouseLeave={() => setMenuOpen(null)}
+                                >
+                                    <Nav.Link href="#" className="menu-link">
+                                        Blog
+                                    </Nav.Link>
+                                    {menuOpen === 4 && (
+                                        <div className="submenu d-flex">
+                                            <div className="submenu-item">
+                                                <a href="" className="text-dark text-decoration-none">Option 1</a>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
+                                <div
+                                    className="nav-item"
+                                    onMouseEnter={() => setMenuOpen(5)}
+                                    onMouseLeave={() => setMenuOpen(null)}
+                                >
+                                    <Nav.Link href="#" className="menu-link">
+                                        Contact
+                                    </Nav.Link>
+                                    {menuOpen === 5 && (
+                                        <div className="submenu d-flex">
+                                            <div className="submenu-item">
+                                                <a href="" className="text-dark text-decoration-none">Option 1</a>
+                                            </div>
+                                        </div>
+                                    )}
+                                </div>
                             </Nav>
                             <Button variant="danger" className="contact-btn">
                                 Get in Touch
