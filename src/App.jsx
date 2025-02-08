3d264fc05200673e5390d22696bd94f77b27e9ef
Nikita Kose <nikita@thepixelbot.com>
2025-02-08 10:25:34 +0530

    removed unwanted code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { useState } from 'react'
 import Footer from './common/Footer'
-import Header from './common/Header'
 import Home from './components/home/Home'
 import AboutUs from './components/aboutUs/AboutUs';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-
       <Router>
-        {/* <Header /> */}
         <Routes>
           <Route path="/" element={<Home />} /> 
           <Route path="/about-us" element={<AboutUs></AboutUs>} /> 
-          
         </Routes>
         <Footer /> 
       </Router>
-
     </>
   )
 }
